80ce9ef0a0904c74c79d8ea62e5a1f8bf51d5cd9
Luís Eduardo <leduardo.na@gmail.com>
2019-11-26 14:30:48 -0300

    feat: Load after auth

--- gobarbermobile/src/pages/SignUp/index.js
+++ gobarbermobile/src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Background from '~/components/Background';
 import { signUpRequest } from '~/store/modules/auth/actions';
@@ -24,6 +24,8 @@ export default function SignUp({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
 
   const handleSubmit = () => {
     dispatch(signUpRequest(name, email, password));
@@ -70,7 +72,9 @@ export default function SignUp({ navigation }) {
             onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={handleSubmit}>Cadastrar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Cadastrar
+          </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
